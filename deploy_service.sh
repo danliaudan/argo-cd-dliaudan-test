4d5ccda5cb36d2836442ac7ce3ab8b90f9159449
liaudanskyidanylo <danylo@drawer.ai>
2023-09-01 13:49:48 +0300

    Updated helm values

--- deploy_service.sh
+++ deploy_service.sh
@@ -2,3 +2,10 @@
 
 # Script for automatic commit and push changes to your github repo
 
+echo Editing values
+yq e -i '.deployment.numReplicas = 4' values.yaml
+
+echo Push to github repository
+git add .
+git commit -m "Updated helm values"
+git push
